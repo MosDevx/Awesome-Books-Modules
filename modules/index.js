75d0b6a9c32d4d3ea7dd33d80b776290bff31816
MosDevx <mosesk.wamae@gmail.com>
2022-11-02 07:25:54 +0300

    Add index

---
+++ modules/index.js
@@ -0,0 +1,14 @@
+import getCurrentTime from './modules/time.js';
+import spaNavigation from './modules/spa-navigation.js';
+import displayBooks from './modules/Book.js';
+
+const timeDisplay = document.getElementById('time-display');
+
+function showTime() {
+  timeDisplay.textContent = getCurrentTime();
+}
+
+setInterval(showTime, 1000);
+
+displayBooks();
+spaNavigation();
